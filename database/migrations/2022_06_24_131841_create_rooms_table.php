c80d1cf401ab00a3c7b511f5c9350f1d9a19f46f
akicacoder <anja99nik@gmail.com>
2022-06-24 15:31:00 +0200

    Kreirane migracije i prazne model, factory i seeder klase

---
+++ database/migrations/2022_06_24_131841_create_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('capacity');
+            $table->integer('floor');
+            $table->integer('room_number');
+            $table->boolean('sea_view');
+            $table->decimal('price');
+            $table->foreignId('hotel_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
