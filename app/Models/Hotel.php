e175ad740043bdb46b3dc67f02b1a347d673e01f
akicacoder <anja99nik@gmail.com>
2022-06-24 15:34:53 +0200

    Povezani modeli i dodat fillable property

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Hotel extends Model
 {
     use HasFactory;
+    protected $fillable = ['place_id', 'name', 'address', 'stars', 'email', 'phone'];
+    protected $with = ['place'];
+    public function place()
+    {
+        return $this->belongsTo(Place::class);
+    }
 }
--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     use HasFactory;
+    protected $fillable = ['hotel_id', 'price', 'sea_view', 'room_number', 'floor', 'capacity'];
+    protected $with = ['hotel'];
+
+    public function hotel()
+    {
+        return $this->belongsTo(Hotel::class);
+    }
 }
